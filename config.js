44d31d69d29811ebb6a11051b2ca13c00c17c62d
Caroline Forslund <cf222ev@student.lnu.se>
2018-03-06 17:44:05 +0100

    Git ignore file update

---
+++ config.js
@@ -0,0 +1,2 @@
+// In this file, you’ll assign an object literal to the module.exports object,
+// as shown in the Twit documentation. The object literal should have the following properties with their corresponding values from your Twitter application account:
