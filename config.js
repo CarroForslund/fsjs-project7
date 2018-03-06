cd65dcf6961e095437ef02fcc548cba375dded56
Caroline Forslund <cf222ev@student.lnu.se>
2018-03-06 17:53:33 +0100

    change

--- config.js
+++ config.js
@@ -1,2 +1,6 @@
 // In this file, you’ll assign an object literal to the module.exports object,
 // as shown in the Twit documentation. The object literal should have the following properties with their corresponding values from your Twitter application account:
+// consumer_key
+// consumer_secret
+// access_token
+// access_token_secret
