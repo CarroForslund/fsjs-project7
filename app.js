35bb3d9046be6a86493e80dc6c2beb4f2d3ac120
Caroline Forslund <cf222ev@student.lnu.se>
2018-03-13 14:44:51 +0000

    Can get user's profile information and render it in index.pug

--- app.js
+++ app.js
@@ -12,32 +12,42 @@ app.set('view engine', 'pug'); //Set up the engine template to use Pug
 //Declare a route. The get methos takes 2 parameters. The route and a cb function
 //The send methos sends a string to the client
 app.get('/', (req, res) => {
-  res.render('index');
+  // ========================
+  // MOVE THIS TO APP.GET "/"
+  // ========================
+  // Twit has promise support; you can use the callback API,
+  // promise API, or both at the same time.
+  T.get('account/verify_credentials', { skip_status: true })
+    .catch(function (err) {
+      console.log('caught error', err.stack)
+    })
+    .then(function (result) {
+      // `result` is an Object with keys "data" and "resp".
+      // `data` and `resp` are the same objects as the ones passed
+      // to the callback.
+      // See https://github.com/ttezel/twit#tgetpath-params-callback
+      // for details.
+      const data = result.data;
+      const user = {};
+      user.name = data.screen_name; //Get profile name
+      user.image = data.profile_image_url; //Get profile image
+      user.background = data.profile_background_image_url; //Get background image
+      // Get the 5 most recent tweets
+      // Get the 5 most recent friends (people I follow)
+      T.get('friends/ids', { screen_name: user.name, count: 5 },  function (err, data, response) {
+        user.friends = data;
+        console.log(user);
+        res.locals.user = user;
+        res.render('index');
+        // console.log(data)
+      });
+      // Get the 5 most recent private messages
+
+
+    });
+
 });
 
 app.listen(3000, () => {
   console.log('The application is running on localhost:3000');
 }); // The app listens to port 3000 (localhost)
-
-// Twit has promise support; you can use the callback API,
-// promise API, or both at the same time.
-T.get('account/verify_credentials', { skip_status: true })
-  .catch(function (err) {
-    console.log('caught error', err.stack)
-  })
-  .then(function (result) {
-    // `result` is an Object with keys "data" and "resp".
-    // `data` and `resp` are the same objects as the ones passed
-    // to the callback.
-    // See https://github.com/ttezel/twit#tgetpath-params-callback
-    // for details.
-    const data = result.data;
-    const profileName = data.screen_name; //Get profile name
-    const profileImg = data.profile_image_url; //Get profile image
-    const profileBg = data.profile_background_image_url; //Get background image
-    // Get the 5 most recent tweets
-    // Get the 5 most recent friends
-    // Get the 5 most recent private messages
-    console.log(profileName);
-    //console.log('data', result.data);
-  })
