8b9bc54cabf771c59426a023313e027bc4682f5f
Caroline Forslund <cf222ev@student.lnu.se>
2018-03-17 08:46:38 +0100

    New approach in app.js plus basic error page added

--- app.js
+++ app.js
@@ -5,56 +5,149 @@ const app = express();  // Express app declaration
 const Twit = require('twit'); //Require Twit to be able to use it
 const config = require('./config');
 const T = new Twit(config);
+const user = {};
 
 //APP (EXPRESS) SETTINGS
 app.set('view engine', 'pug'); //Set up the engine template to use Pug
 app.use(express.static('public'));
+
+// app.use((req, res, next) => {
+//   // Twit has promise support; you can use the callback API,
+//   // promise API, or both at the same time.
+//   T.get('account/verify_credentials', { skip_status: true })
+//     .catch(function (err) {
+//       console.log('caught error', err.stack)
+//     })
+//     .then(function (result) {
+//       // `result` is an Object with keys "data" and "resp".
+//       // `data` and `resp` are the same objects as the ones passed
+//       // to the callback.
+//       // See https://github.com/ttezel/twit#tgetpath-params-callback
+//       // for details.
+//       const data = result.data;
+//       // console.log(data);
+//       user.id = data.id;
+//       user.name = data.screen_name; //Get profile name
+//       user.image = data.profile_image_url; //Get profile image
+//       user.background = data.profile_banner_url; //Get background image
+//
+//       // Get the 5 most recent tweets
+//       T.get('statuses/user_timeline', { screen_name: user.name, count: 5 },  function (err, data, res) {
+//         user.tweets = data;
+//       });
+//
+//       T.get('friends/list', { screen_name: user.name, count: 5 },  function (err, data, res) {
+//
+//         if(err){
+//           console.log('Error: ', err);
+//         }
+//         else {
+//           user.friends = data;
+//           // console.log(user.friends);
+//         }
+//
+//       });
+//
+//       // Get the 5 latest private messages in my last private conversation
+//       T.get('direct_messages/events/list', { screen_name: user.name, count: 5 },  function (err, data, response) {
+//         console.log(data);
+//         user.messages = data.event;
+//
+//         // console.log(user.messages[0].message_create.message_data);
+//       });
+//
+//     });
+//     next();
+// });
+app.use((req, res, next) => {
+  // Twit has promise support; you can use the callback API,
+  // promise API, or both at the same time.
+  T.get('account/verify_credentials', { skip_status: true }, function () {
+    const data = res;
+    console.log(data);
+    user.id = data.id;
+    user.name = data.screen_name; //Get profile name
+    user.image = data.profile_image_url; //Get profile image
+    user.background = data.profile_banner_url; //Get background image
+  });
+  next();
+
+}, (req, res, next) => {
+
+  // Get the 5 most recent tweets
+  T.get('statuses/user_timeline', { screen_name: user.name, count: 5 },  function (err, data, res) {
+    user.tweets = data;
+  });
+  next();
+
+}, (req, res, next) => {
+
+  T.get('friends/list', { screen_name: user.name, count: 5 },  function (err, data, res) {
+
+    if(err){
+      console.log('Error: ', err);
+    }
+    else {
+      user.friends = data;
+      // console.log(user.friends);
+    }
+  });
+    next();
+
+}, (req, res, next) => {
+  // Get the 5 latest private messages in my last private conversation
+  T.get('direct_messages/events/list', { screen_name: user.name, count: 5 },  function (err, data, response) {
+    // console.log(data);
+    user.messages = data.event;
+
+    // console.log(user.messages[0].message_create.message_data);
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  // Get the 5 most recent tweets
+  T.get('statuses/user_timeline', { screen_name: user.name, count: 5 },  function (err, data, res) {
+    user.tweets = data;
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  // Get my 5 latest friends (people I follow)
+  T.get('friends/list', { screen_name: user.name, count: 5 },  function (err, data, res) {
+
+    if(err){
+      console.log('Error: ', err);
+    }
+    else {
+      user.friends = data;
+      console.log(user.friends);
+    }
+
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  // Get the 5 latest private messages in my last private conversation
+  T.get('direct_messages/events/list', { screen_name: user.name, count: 5 },  function (err, data, response) {
+    user.messages = data.events;
+    // console.log(user.messages.message_create.message_data);
+  });
+  next();
+});
+
 //Declare a route. The get methos takes 2 parameters. The route and a cb function
 //The send methos sends a string to the client
 app.get('/', (req, res) => {
-  // Twit has promise support; you can use the callback API,
-  // promise API, or both at the same time.
-  T.get('account/verify_credentials', { skip_status: true })
-    .catch(function (err) {
-      //TODO: Redirect to 404 page
-      console.log('caught error', err.stack)
-    })
-    .then(function (result) {
-      // `result` is an Object with keys "data" and "resp".
-      // `data` and `resp` are the same objects as the ones passed
-      // to the callback.
-      // See https://github.com/ttezel/twit#tgetpath-params-callback
-      // for details.
-      const data = result.data;
-      const user = {};
-      user.name = data.screen_name; //Get profile name
-      user.image = data.profile_image_url; //Get profile image
-      user.background = data.profile_banner_url; //Get background image
+  res.locals.user = user;
+  res.render('index');
+});
 
-      // console.log(data);
-      // Get the 5 most recent tweets
-      // statuses/user_timeline
-      T.get('statuses/user_timeline', { screen_name: user.name, count: 5 },  function (err, data, response) {
-        user.tweets = data;
-        // res.locals.user = user;
-        // res.render('index');
-        // console.log('friends', user.tweets)
-      });
-
-      // Get my 5 latest friends (people I follow)
-      T.get('friends/list', { screen_name: user.name, count: 5 },  function (err, data, response) {
-        user.friends = data;
-        // console.log(user.friends.users[0]);
-        res.locals.user = user;
-        res.render('index');
-        console.log(data)
-      });
-
-      // Get the 5 most recent private messages
-
-
-    });
-
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.render('error');
 });
 
 app.listen(3000, () => {
